2ee015b343c0ef6f2cde9d3ac32ea72573e8ef44
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-08-15 11:47:57 +0900

    Add files via upload

---
+++ save_vector.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.schema.document import Document
+
+embed_model_id = "pkshatech/GLuCoSE-base-ja"
+embeddings = HuggingFaceEmbeddings(model_name=embed_model_id)
+
+text_files = Path("textdata").glob("*.txt")
+
+text_data = []
+for text_file in text_files:
+    with open(text_file.as_posix(), "r", encoding="utf-8") as f:
+        text_data.append(f.read())
+
+docs = [Document(page_content=x) for x in text_data]
+
+db = FAISS.from_documents(docs, embeddings)
+
+db.save_local("faiss_index")
