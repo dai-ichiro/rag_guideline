322911c347ff3b43116bdd4ff9cb150d435ee31f
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-08-21 20:49:28 +0900

    Add files via upload

--- run_rag.py
+++ run_rag.py
@@ -1,8 +1,12 @@
+import torch
+from janome.tokenizer import Tokenizer
 import gradio as gr
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 from threading import Thread
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers.ensemble import EnsembleRetriever
 
 system_prompt_base = '''あなたは親切なAIアシスタントです。以下の与えらえた文章を参照してユーザーの質問にできるだけ丁寧に日本語で回答して下さい。与えられた文章に記載されていない内容の質問には「わかりません」と回答して下さい。
 
@@ -16,13 +20,30 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained("cyberagent/calm3-22b-chat")
 streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
+## faiss_retriever
 embed_model_id = "pkshatech/GLuCoSE-base-ja"
 embeddings = HuggingFaceEmbeddings(model_name=embed_model_id)
 
-db = FAISS.load_local(
-    "faiss_index",
-    embeddings=embeddings,
-    allow_dangerous_deserialization=True
+docs = torch.load("docs.langchaindata", weights_only=False)
+faiss_retriever = FAISS.from_documents(docs, embeddings).as_retriever()
+
+## bm25_retriever
+t = Tokenizer()
+def preprocess_jp(text: str) -> list[str]:
+    tokenized_words = [token.surface for token in t.tokenize(text)]
+    return tokenized_words
+
+vectorizer = torch.load("vectorizer.langchaindata", weights_only=False)
+
+bm25_retriever = BM25Retriever(
+    vectorizer = vectorizer,
+    preprocess_func=preprocess_jp,
+    docs = docs
+)
+
+ensemble_retriever = EnsembleRetriever(
+    retrievers=[bm25_retriever, faiss_retriever],
+    weights=[0.5, 0.5]
 )
 
 def call_llm(
@@ -34,10 +55,9 @@ def call_llm(
 ):
     history_openai_format = []
 
-    query = message
-    docs = db.similarity_search(query, k=1)
-    context = docs[0].page_content
-    metadata_ref = docs[0].metadata["source"]
+    result = ensemble_retriever.invoke(message)
+    context = result[0].page_content
+    metadata_ref = result[0].metadata["source"]
     system_prompt = system_prompt_base.format(context=context)
     init = {
         "role": "system",
