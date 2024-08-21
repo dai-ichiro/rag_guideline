f64cd72f2cd1050ae05b6b0697b18e4d25182fff
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2024-08-21 11:06:44 +0900

    Update save_vector.py

--- save_vector.py
+++ save_vector.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
